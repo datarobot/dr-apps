d73b9bad1cedc8fa89f5c154546f13a702c81d0e
Illia Demchuk <illia.demchuk@datarobot.com>
2023-11-21 13:47:47 +0200

    Fixed trees

---
+++ prediction-demo/dr_streamlit/__init__.py
@@ -0,0 +1,11 @@
+from .caches import get_model, get_project, get_deployment
+from .create_prediction import create_prediction_form
+from .feature_histogram import feature_histogram_chart
+from .feature_impact import derived_features_chart
+from .prediction_display_widget import prediction_display_chart
+from .prediction_distribution import prediction_distribution_chart
+from .prediction_explanations_table import prediction_explanation_table
+from .predictor import submit_batch_prediction, submit_prediction, get_distribution_chart_data
+from .select_box import multiclass_dropdown_menu, text_feature_dropdown_menu, project_model_dropdown
+from .wordcloud import wordcloud_chart
+from .experiment_overview import experiment_container_overview_widget
