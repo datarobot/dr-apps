82da019083d54d3681f97f1e7bb24f182a92f1b9
Amod Dhopavkar <amod.dhopavkar@datarobot.com>
2024-06-25 10:25:46 -0400

    update pr

--- examples/prediction-demo/insights_app_example.py
+++ examples/prediction-demo/insights_app_example.py
@@ -5,8 +5,7 @@ from dr_streamlit import (
     derived_features_chart,
     multiclass_dropdown_menu,
     project_model_dropdown,
-    text_feature_dropdown_menu,
-    wordcloud_chart,
+    text_feature_dropdown_menu, wordcloud_chart,
 )
 from datarobot import Client
 from datarobot.client import set_client
