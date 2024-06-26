4bf899cb414c148a884b89f95709dff8329470f9
Amod Dhopavkar <amod.dhopavkar@datarobot.com>
2024-06-26 15:11:30 -0400

    add metadata.yaml

--- drapps/helpers/exceptions.py
+++ drapps/helpers/exceptions.py
@@ -6,12 +6,15 @@
 #  Released under the terms of DataRobot Tool and Utility Agreement.
 #
 
+from typing import List
+
 
 class ClientResponseError(Exception):
-    def __init__(self, url: str, status: int, message: str):
+    def __init__(self, url: str, status: int, message: str, errors: List[str] = None):
         self.url = url
         self.status = status
         self.message = message
+        self.errors = errors
 
     def __str__(self) -> str:
         return f'{self.status}, message={self.message}, url={self.url}'
