b03a75ab36ab133c309d6803f8e664eb697cb858
Benjamin Cutler <benjamin.cutler@datarobot.com>
2025-03-18 08:54:10 -0400

    [-] Supply a better error message for execution env failing to build

--- drapps/create.py
+++ drapps/create.py
@@ -281,10 +281,10 @@ def wait_for_execution_environment_version_ready(
             sleep(CHECK_STATUS_WAIT_TIME)
         else:
             raise RuntimeError(
-                f'Image build failed with status {img_status} after {runs * CHECK_STATUS_WAIT_TIME} seconds'
+                f'Build for execution environment version failed with status {img_status} after {runs * CHECK_STATUS_WAIT_TIME} seconds'
             )
     if img_status in IMAGE_BUILD_FAILED_STATUSES:
-        raise Exception("Image build failed")
+        raise Exception(f"Build for execution environment version failed with status {img_status}")
 
 
 def send_docker_image_with_progress(
